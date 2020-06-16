99449a13053fd015c6b8a5635432d69915e50406
Pietro-Tar97 <pietro.tarasco@hotmail.it>
2020-06-16 15:40:31 +0200

    init class Actors

--- src/main/java/Actors/Movimento.java
+++ src/main/java/Actors/Movimento.java
@@ -1,13 +1,32 @@
 package Actors;
 
-public class Movimento {
+import java.util.Date;
+
+import IActors.IMovimento;
+
+public class Movimento implements IMovimento{
 
 	private int id;
-	private int numeroConto;
 	private double sommaDenaro;
 	private String tipo;
+	private Date dataMovimento;
 	
-	public Movimento() {
-		
+	public Movimento(int id,double sommaDenaro,String tipo,Date dataMovimento) {
+		this.id=id;
+		this.sommaDenaro=sommaDenaro;
+		this.tipo=tipo;
+		this.dataMovimento=dataMovimento;
+	}
+
+	@Override
+	public double getSommaMovimento() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getTipoMovimento() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
